218ad762b0727a4f33b208381aa07582a567afb3
princesharma7300 <prince.gagan2016@gmail.com>
2022-08-27 12:27:03 +0530

    L2-Bind the attribute

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            images:"./assets/images/socks_blue.jpg",
+            
         }
     }
 })
